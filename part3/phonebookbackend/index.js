e80641eac35126508d39d00f36fb6d25f1aac881
Jonroi <joni.roine@outlook.com>
2024-04-16 15:07:41 +0300

    copied phonebook repo

---
+++ part3/phonebookbackend/index.js
@@ -0,0 +1,132 @@
+const express = require('express');
+
+const app = express();
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const Person = require('./models/person');
+
+app.use(express.static('dist'));
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+};
+
+// Error handler
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+const cors = require('cors');
+
+app.use(cors()); // CORS middleware
+app.use(express.json()); // JSON middleware
+app.use(requestLogger); // request logger middleware
+
+// Error handling for unknown endpoint
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+// main page route
+app.get('/', (request, response) => {
+  response.send('<h1>Hello Phonebook!</h1>');
+});
+
+// get all persons
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
+});
+
+// create a new person
+app.post('/api/persons', (request, response, next) => {
+  const { body } = request;
+
+  // checks if name or number is missing
+  if (!body.name || !body.number) {
+    return response.status(400).json({ error: 'Name or number is missing' });
+  }
+
+  // Validation for name length
+  if (body.name.length < 3) {
+    return response
+      .status(400)
+      .json({ error: 'Name must be at least 3 characters long' });
+  }
+
+  // Validation for phone number format
+  const phoneNumberPattern = /^(0\d{1,3}-\d{7,})$/;
+  if (!phoneNumberPattern.test(body.number)) {
+    return response.status(400).json({ error: 'Invalid phone number format' });
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+// get a single person
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+// delete a person
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+// update a person
+app.put('/api/persons/:id', (request, response, next) => {
+  const { body } = request;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
+
+const { PORT } = process.env;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
