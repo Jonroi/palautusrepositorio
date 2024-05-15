6965760d3e18f4f112bad675c48cadbd510a257d
Jonroi <joni.roine@outlook.com>
2024-05-15 17:00:09 +0300

    note redux

--- part6/redux-notes/src/reducers/noteReducer.js
+++ part6/redux-notes/src/reducers/noteReducer.js
@@ -1,24 +1,34 @@
-const noteReducer = (state = [], action) => {
-  switch(action.type) {
+const initialState = [
+  {
+    content: 'reducer defines how redux store works',
+    important: true,
+    id: 1
+  },
+  {
+    content: 'state of store can contain any data',
+    important: false,
+    id: 2
+  }
+]
+
+const noteReducer = (state = initialState, action) => {
+  switch (action.type) {
     case 'NEW_NOTE':
       return [...state, action.payload]
     case 'TOGGLE_IMPORTANCE':
       const id = action.payload.id
-      const noteToChange = state.find(n => n.id === id)
-      const changedNote = { 
-        ...noteToChange, 
-        important: !noteToChange.important 
+      const noteToChange = state.find((n) => n.id === id)
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important
       }
-      return state.map(note =>
-        note.id !== id ? note : changedNote 
-      )
+      return state.map((note) => (note.id !== id ? note : changedNote))
     default:
       return state
-    }
-  } 
+  }
+}
 
-const generateId = () =>
-  Number((Math.random() * 1000000).toFixed(0))
+const generateId = () => Number((Math.random() * 1000000).toFixed(0))
 
 export const createNote = (content) => {
   return {
@@ -36,6 +46,6 @@ export const toggleImportanceOf = (id) => {
     type: 'TOGGLE_IMPORTANCE',
     payload: { id }
   }
-}  
+}
 
 export default noteReducer
