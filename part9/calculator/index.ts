b34ad71a4a296c8e6c504b75c1a704dc937d539a
Joni Roine <joni.roine@outlook.com>
2024-05-23 15:44:54 +0300

    eslint conf

--- part9/calculator/index.ts
+++ part9/calculator/index.ts
@@ -1,19 +1,42 @@
 import express from 'express'
-import { calculator } from './calculator'
+import { calculator, Operation } from './calculator'
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/hello', (_request, response) => {
-  response.send('Hello Full Stack!')
+app.get('/hello', (_reg, res) => {
+  res.send('Hello Full Stack!')
 })
 
 app.post('/calculate', (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const { value1, value2, op } = req.body
 
-  const result = calculator(value1, value2, op)
-  res.send({ result })
+  if (!value1 || isNaN(Number(value1))) {
+    return res.status(400).send({ error: 'malformatted parameters for value1' })
+  }
+
+  if (!value2 || isNaN(Number(value2))) {
+    return res.status(400).send({ error: 'malformatted parameters for value2' })
+  }
+
+  const validOps = ['add', 'subtract', 'multiply', 'divide']
+  if (!op || !validOps.includes(op)) {
+    return res
+      .status(400)
+      .send({ error: 'malformatted parameters for operation' })
+  }
+
+  try {
+    const operation = op as Operation
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const result = calculator(Number(value1), Number(value2), operation)
+
+    res.send({ result })
+  } catch (error) {
+    res.status(400).send({ error: error.message })
+  }
 })
 
 const Port = 3003
