e0bd27b8d7ae975ebd0575e9b78e0faa9e168121
Joni Roine <joni.roine@outlook.com>
2024-06-01 13:35:17 +0300

    routing / service

--- part9/flightDiaries/src/index.ts
+++ part9/flightDiaries/src/index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import router from './routes/diaries';
+
 const app = express();
 app.use(express.json());
 
 const PORT = 3000;
+
+app.use('/api/diaries', router);
+console.log('fetching diaries');
 
 app.get('/ping', (_req, res) => {
   console.log('someone pinged here');
